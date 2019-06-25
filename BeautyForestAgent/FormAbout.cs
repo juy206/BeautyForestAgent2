16fa2e6de53e4cf668a2c48b2cec9a73f7cef8fc
Mirim <Mirim@DESKTOP-Q47BHVD>
2019-06-25 19:12:17 +0900

    #2 Modal & Modaless 창 띄우기 코드 구현 by 3614 정준명

--- BeautyForestAgent/FormAbout.cs
+++ BeautyForestAgent/FormAbout.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 namespace BeautyForestAgent
 {
     public partial class FormAbout : Form
-    {
+ {
         public FormAbout()
         {
             InitializeComponent();
